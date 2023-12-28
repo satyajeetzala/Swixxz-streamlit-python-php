424c5ed7aee2e66843e3406300487c1bdf2d9f61
satyajeet <szala255@gmail.com>
2023-12-28 11:23:10 +0530

    index page changes

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 </head>
 
-<body class="font-sans bg-gray-100 relative">
+<body class="font-sans relative">
 
     <!-- Banner Image -->
     <div id="navbar">
@@ -61,7 +61,34 @@
         </div>
     </div>
 
-   
+
+
+    
+
+    <div id="home">
+    <section class="text-gray-600 body-font" >
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center mt-20" style="background-color:#000">
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
+          </div>
+          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to My Website
+            </h1>
+            <p class="mb-8 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nam euismod maximus varius. Cras id dui ut ligula volutpat tristique. 
+                Ut mauris ex, auctor vitae aliquet id, scelerisque ut nibh.</p>
+            <div class="flex justify-center">
+              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+            </div>
+          </div>
+        </div>
+      </section>
+  </div>
+
+  
+ 
+  <iframe class="w-full h-96" src="http://localhost:8502/" title="Streamlit App" frameborder="0" allowfullscreen></iframe>
 
 <footer class="" style="background-color: rgb(0, 0, 0,1)">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
